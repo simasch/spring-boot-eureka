61a8ec5e3b2bb0c895d2bfbfeb388141b3533b20
Simon Martinelli <simon@72.services>
2019-03-21 10:12:06 +0100

    'testing'

--- client2/src/main/java/eureka/client2/ServiceInstanceRestController.java
+++ client2/src/main/java/eureka/client2/ServiceInstanceRestController.java
@@ -3,6 +3,7 @@ package eureka.client2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,13 @@ public class ServiceInstanceRestController {
     @RequestMapping("/service-instances/{applicationName}")
     public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
         return this.discoveryClient.getInstances(applicationName);
+    }
+
+    @GetMapping("/master")
+    public String master() {
+        List<ServiceInstance> master = this.discoveryClient.getInstances("master");
+        ServiceInstance serviceInstance = master.get(0);
+
+        return serviceInstance.getHost() + ":" + serviceInstance.getPort();
     }
 }
